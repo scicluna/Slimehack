4ce4ae648ac2ff4b6ef719e640abf0adcbff0fe7
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-02 10:33:16 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors({
     credentials: true
   }));
 
-  app.use(express.static(path.join(__dirname, 'src')));
+  app.use(express.static('src'));
   
   async function startServer() {
     try {
